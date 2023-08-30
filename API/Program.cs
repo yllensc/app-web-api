1308eb5dc692270ae1a185ca16a02df647336879
yllensc <yllensc18@gmail.com>
2023-08-30 16:26:48 -0500

    feat: :construction: Inyección del DBContext a program

--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using Persistencia;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//DBContext
+builder.Services.AddDbContext<AppWebApiContext>(options =>
+{
+    string? ConnectionStrings = builder.Configuration.GetConnectionString("ConexMySql");
+    options.UseMySql(ConnectionStrings, ServerVersion.AutoDetect(ConnectionStrings));
+});
 
 var app = builder.Build();
 
