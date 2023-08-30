934e67f6ff2e2df55f27dfff0bc3f72ee3d30686
yllensc <yllensc18@gmail.com>
2023-08-30 16:14:49 -0500

    feat: :construction: Listo el archivo para setear las entidades (DBContext jiji)

---
+++ Persistencia/AppWebApiContext.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using Dominio.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistencia;
+    public class AppWebApiContext : DbContext
+    {
+        public AppWebApiContext(DbContextOptions<AppWebApiContext> options): base(options){
+
+        }
+        public DbSet<Pais> Paises { get; set;}
+        public DbSet<Estado> Estados { get; set;}
+        public DbSet<Region> Regiones { get; set;}
+        public DbSet<Persona> Personas { get; set;}
+        public DbSet<TipoPersona> TipoPersonas { get; set;}
+        public DbSet<Genero> Generos { get; set;}
+        public DbSet<Matricula> Matriculas { get; set;}
+        public DbSet<Salon> Salones { get; set;}
+        public DbSet<TrainerSalon> TrainersSalones { get; set;}
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder){
+            modelBuilder.Entity<Matricula>().HasKey(p => new{p.IdPersonaFK, p.IdSalonFK});
+            modelBuilder.Entity<TrainerSalon>().HasKey(p => new{p.IdPersonaFK, p.IdSalonFK});
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+}
